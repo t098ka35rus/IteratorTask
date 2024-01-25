b2c4da6abe68650b2c9d6b228abdebda5f2bd162
Timofey Zab <tim18125@gmail.com>
2024-01-25 23:38:35 +0300

    Initial commit

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+    public Randoms(int min, int max) {
+        //..
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(min, max);
+            }
+        };
+    }
+}
+
